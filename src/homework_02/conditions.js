2aaf70e84d9afc6b7754416c66ed0b60975c8f8f
PARKER\Parker <parkers@mail.ru>
2021-08-11 23:29:20 +0300

    homework_02

---
+++ src/homework_02/conditions.js
@@ -0,0 +1,38 @@
+export function getLarger(a, b) {
+  if (a > b) {
+    console.log(a);
+  } else {
+    console.log(b);
+  }
+}
+
+export function getMonth() {
+  const digit = prompt("Введите число от 1 до 12:");
+  if (digit > 0 && digit < 13) {
+    const months = [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ];
+    console.log(months[digit - 1]);
+  } else {
+    console.log(0);
+  }
+}
+
+export function getCompare(circle, square) {
+  if (Math.sqrt((4 * circle) / 3.14) < Math.sqrt(square)) {
+    console.log("Поместится");
+  } else {
+    console.log("Не поместится");
+  }
+}
