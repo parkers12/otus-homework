a3c972a5826283bb0e7e822a2eea4f274e7786ec
PARKER\Parker <parkers@mail.ru>
2021-08-16 11:48:21 +0300

    homework_05

---
+++ src/homework_05/array.test.js
@@ -0,0 +1,27 @@
+import { getArrSum, getNewArr, getArrMinMax } from "./array";
+
+describe("getArrSum", () => {
+  const consoleSpy = jest.spyOn(console, "log");
+  it("sum elements array", () => {
+    console.log(getArrSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    expect(consoleSpy).toHaveBeenCalledWith(55);
+  });
+});
+
+describe("getNewArr", () => {
+  const consoleSpy = jest.spyOn(console, "log");
+  it("increase array elements", () => {
+    console.log(getNewArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    expect(consoleSpy).toHaveBeenCalledWith([
+      2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+    ]);
+  });
+});
+
+describe("getArrMinMax", () => {
+  const consoleSpy = jest.spyOn(console, "log");
+  it("Find largest and smallest elements of array", () => {
+    console.log(getArrMinMax([23, 36, 12, 58, 18, 6, 72, 52, 39, 92]));
+    expect(consoleSpy).toHaveBeenCalledWith(6, 92);
+  });
+});
